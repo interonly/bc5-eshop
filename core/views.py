aca368c9245b5bc4254f2cf623e33660b4019087
Эмир Абакиров <abakirovemir2008@gmail.com>
2024-07-15 14:20:11 +0600

    classwork + userpage

--- core/views.py
+++ core/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, HttpResponse
 from .models import Product
 from costumerapp.models import Costumer
-
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 def homepage(request):
     product_list = Product.objects.all()
     context = {"products": product_list}
 
     return render(request, 'index.html', context)
-
 
 def product_detail(request, id):
     product_object = Product.objects.get(id=id)
@@ -33,3 +33,13 @@ def product_detail(request, id):
     }
     return render(request, 'product_detail.html', context)
 
+def users_page(request):
+    User = get_user_model()
+    users = User.objects.all()
+    context = {"users": users}
+    return render(request, 'users.html', context)
+
+def user_detail(request, id):
+    user_data = User.objects.get(id=id)
+    context = {"user": user_data}
+    return render(request, 'user_detail.html', context)
--- eshop/urls.py
+++ eshop/urls.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import homepage, product_detail
+from core.views import homepage, product_detail, users_page, user_detail
 from costumerapp.views import costumer_view
 from news.views import news_view, news_detail
 
 urlpatterns = [
+    #general
     path('admin/', admin.site.urls),
     path('', homepage),
+    #user
+    path('users/', users_page, name='users'),
+    path('users/user/<int:id>/', user_detail, name='user-profile'),
+    #product
     path('product/<int:id>/', product_detail, name='product-detail'),
+    #costumer
     path('costumers/', costumer_view),
+    #news
     path('news/', news_view),
     path('news/<int:id>/', news_detail, name='news-detail'),
 ]
