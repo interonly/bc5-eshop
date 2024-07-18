70b8f950c3899b3a83f4747069181f4e7bd8bfd9
Эмир Абакиров <abakirovemir2008@gmail.com>
2024-07-18 14:37:09 +0600

    classwork + bootstrap redesign

---
+++ core/static/code.js
@@ -0,0 +1,12 @@
+// const navLink = document.querySelector("nav button");
+
+// function changeStyle(e) {
+//     navLink.style.backgroundColor = 'darkred'
+// }
+
+// navLink.addEventListener("mouseover", changeStyle)
+
+// const nav = document.getElementsByTagName("nav")[0]
+// nav.innerHTML += '<a href="/basket/"><button class="button_style">Корзина</button></a>'
+           
+        
